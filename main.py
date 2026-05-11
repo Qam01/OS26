8948f7a6f443ddecab3a2fd77189f736576d6433
Qam01 <ahmad2001q@gmail.com>
2026-05-11 04:02:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import tkinter as tk
+from gui_app import OSSimulatorGUI
 
+def main():
+    root = tk.Tk()
+    app = OSSimulatorGUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
